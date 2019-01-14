ea7025c751b8d2fbb82be6a705aa52b3437bf55f
Miguel de Oliveira Dias Gonçalves <miguel.d.goncalves@tecnico.ulisboa.pt>
2019-01-14 02:22:12 +0100

    Utilización de la base de datos Mongo implementada closes #35

--- src/main/java/CC1819/Dao.java
+++ src/main/java/CC1819/Dao.java
@@ -2,8 +2,11 @@ package CC1819;
 
 import java.util.ArrayList;
 
-import com.mongodb.client.MongoDatabase; 
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
 import com.mongodb.MongoClient;
+import org.bson.Document;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -15,20 +18,28 @@ public class Dao {
 	public static final String VIAJES_COLLECTION = "viajes";
 	public static final String NOTICIAS_COLLECTION = "noticias";
 	
-	private static Dao dao = null;
+	public static final String ID_COLUMN = "id";
+	public static final String ORIGEN_COLUMN = "origen";
+	public static final String DESTINO_COLUMN = "destino";
+	public static final String PARTIDA_COLUMN = "partida";
+	public static final String LLEGADA_COLUMN = "llegada";
+	public static final String PRECIO_COLUMN = "precio";
+	public static final String NOTICIA_COLUMN = "noticia";
 	
-	private ArrayList<DataObject> viajes = new ArrayList<>();
-	private ArrayList<String> noticias = new ArrayList<>();
+	private static Dao dao = null;
 	
 	private MongoDatabase db = null;
 	
-	private AtomicInteger counter = new AtomicInteger(1);
+	private AtomicInteger counterViajes = new AtomicInteger(0);
+	private AtomicInteger counterNoticias = new AtomicInteger(0);
 	
 	private Dao() {
 		
 	}
 	
 	private void setDao() {
+		
+		setDatabase();
 		
 		postViaje("Granada", "Maracena", "08h04", "08h10", 1.50);
 		postViaje("Granada", "Armilla", "09h16", "09h26", 1.50);
@@ -37,8 +48,6 @@ public class Dao {
 		postNoticia("1.000 dias sin tren de Granada a Madrid");
 		postNoticia("Habra Talgo de Granada a Madrid");
 		postNoticia("Podra haber Tren Hotel de Granada a Barcelona?");
-		
-		setDatabase();
 
 	}
 	
@@ -56,41 +65,82 @@ public class Dao {
 	}
 	
 	public void postViaje(String origen, String destino, String partida, String llegada, double precio) {
-		this.viajes.add(new DataObject(origen, destino, partida, llegada, precio));
+		
+		counterViajes.incrementAndGet();
+		
+		// Documents funcionan como lineas de una tabla en una base de datos relacional
+		Document document = new Document(ID_COLUMN, counterViajes)
+				.append(ORIGEN_COLUMN, origen)
+				.append(DESTINO_COLUMN, destino)
+				.append(PARTIDA_COLUMN, partida)
+				.append(LLEGADA_COLUMN, llegada)
+				.append(PRECIO_COLUMN, precio);
+		db.getCollection(VIAJES_COLLECTION).insertOne(document);
 	}
 	
 	public void postNoticia(String texto) {
-		this.noticias.add(texto);
+		
+		counterNoticias.incrementAndGet();
+		
+		Document document = new Document(ID_COLUMN, counterNoticias)
+				.append(NOTICIA_COLUMN, texto);
+		db.getCollection(NOTICIAS_COLLECTION).insertOne(document);
 	}
 	
 	public DataObject findViajeById(int id) {
-		if (0<=id-1 && id-1<viajes.size())
-			return this.viajes.get(id-1);
+		if (0<id && id<=counterViajes.get()) {
+			MongoCursor<Document> cursor = db.getCollection(VIAJES_COLLECTION).find(Filters.eq(ID_COLUMN, id)).iterator();
+			if(cursor.hasNext()) {
+				Document document = cursor.next();
+				String origen = document.getString(ORIGEN_COLUMN);
+				String destino = document.getString(DESTINO_COLUMN);
+				String partida = document.getString(PARTIDA_COLUMN);
+				String llegada = document.getString(LLEGADA_COLUMN);
+				double precio = document.getDouble(PRECIO_COLUMN);
+				cursor.close();
+				return new DataObject(origen, destino, partida, llegada, precio);
+			}
+			cursor.close();
+		}
 		return null;
 	}
 	
 	public String findNoticiaById(int id) {
-		if (0<=id-1 && id-1<noticias.size())
-			return this.noticias.get(id-1);
+		if (0<id && id<=counterNoticias.get()) {
+			MongoCursor<Document> cursor = db.getCollection(NOTICIAS_COLLECTION).find(Filters.eq(ID_COLUMN, id)).iterator();
+			if(cursor.hasNext()) {
+				Document document = cursor.next();
+				String noticia = document.getString(NOTICIA_COLUMN);
+				cursor.close();
+				return noticia;
+			}
+			cursor.close();
+		}
 		return null;
 	}
 	
 	public ArrayList<DataObject> getAllViajes() {
+		ArrayList<DataObject> viajes = new ArrayList<>();
+		for(int i=1; i<=counterViajes.get(); i++)
+			viajes.add(findViajeById(i));
 		return viajes;
 	}
 	
 	public ArrayList<String> getAllNoticias() {
+		ArrayList<String> noticias = new ArrayList<>();
+		for(int i=1; i<=counterNoticias.get(); i++)
+			noticias.add(findNoticiaById(i));
 		return noticias;
 	}
 	
 	public void deleteViaje(int id) {
-		if (0<=id-1 && id-1<viajes.size())
-			this.viajes.set(id-1, null);
+		if (0<id && id<=counterViajes.get())
+			db.getCollection(VIAJES_COLLECTION).deleteOne(Filters.eq(ID_COLUMN, id));
 	}
 	
 	public void deleteNoticia(int id) {
-		if (0<=id-1 && id-1<noticias.size())
-			this.noticias.set(id-1, null);		
+		if (0<id && id<=counterNoticias.get())
+			db.getCollection(NOTICIAS_COLLECTION).deleteOne(Filters.eq(ID_COLUMN, id));
 	}
 	
 	public static void cleanDao() {
@@ -100,8 +150,8 @@ public class Dao {
 	
 	public void setDatabase() {
 		
-		//La instancia de Mongo correra en http://<HOST> y escuchara en el puerto MONGO_PORT
-		MongoClient mongo = new MongoClient( HOST , MONGO_PORT );
+		//La instancia de Mongo corre en http://<HOST> y escucha en el puerto MONGO_PORT
+		MongoClient mongo = new MongoClient(HOST, MONGO_PORT);
 		// La base de datos es creada automaticamente si no existe
 		db = mongo.getDatabase(DATABASE_NAME);
 		// Collections son como tablas en bases de datos relacionales
