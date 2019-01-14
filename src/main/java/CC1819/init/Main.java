aebb6a74ceedf873b7c3757ffd329963f5c1a841
Miguel de Oliveira Dias Gonçalves <miguel.d.goncalves@tecnico.ulisboa.pt>
2019-01-14 18:31:34 +0100

    Main de la aplicación lista para el nuevo microservicio #36

--- src/main/java/CC1819/init/Main.java
+++ src/main/java/CC1819/init/Main.java
@@ -4,9 +4,30 @@ import CC1819.informacion.JavalinApp;
 
 public class Main {
 	
+	// Decide que servicio o servicios se ejecutaran
+	public static final String VARIABLE_SERVICIO = "SERVICIO";
+	
+	public static final int SERVICIO_INFO = 1;
+	public static final int SERVICIO_VIAJES = 2;
+	public static final int SERVICIO_TODOS = 3;
+	
 	public static void main(String[] args) {
-		JavalinApp app = new JavalinApp();
-		app.init();
+		
+		String servicioString = System.getenv().get(VARIABLE_SERVICIO);
+		int servicio = SERVICIO_INFO; //Valor por defecto
+		if(servicioString!=null)
+			servicio = Integer.parseInt(servicioString);
+		
+		// Arranca el microservicio de informacion al cliente
+		if(servicio==1 || servicio==3) {
+			CC1819.informacion.JavalinApp app = new CC1819.informacion.JavalinApp();
+			app.init();
+		}
+		
+		//Arranca el microservicio de gestion de viajes
+		if(servicio==2 || servicio==3) {
+			
+		}
 	}
 
 }
