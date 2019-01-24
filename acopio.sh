2460812ee4e57f85cf2d3b2d638507ad476f6308
Miguel de Oliveira Dias Gonçalves <miguel.d.goncalves@tecnico.ulisboa.pt>
2019-01-24 13:32:09 +0100

    Comentários reescritos en el script de provisionamiento #37

--- acopio.sh
+++ acopio.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#Script de creación, configuración y provisionamiento de una máquina virtual en Azure
+#Script de creación, configuración y provisionamiento de dos máquinas virtuales en Azure
 
 #Variables
 REGION='uksouth'                                          #Región del centro de datos Azure donde se alojará la aplicación
 GRUPO_NOMBRE='CCGroup'                                    #Nombre del grupo de recursos a crear
-VM_NOMBRE_INFO='CCazureInfo'                              #Nombre a atribuir a la máquina virtual
-VM_NOMBRE_VIAJES='CCazureViajes'                          #Nombre a atribuir a la máquina virtual
+VM_NOMBRE_INFO='CCazureInfo'                              #Nombre a atribuir a la máquina virtual del microservicio de Información al Cliente
+VM_NOMBRE_VIAJES='CCazureViajes'                          #Nombre a atribuir a la máquina virtual del microservicio de Gestión de Viajes
 IMAGEN='Canonical:UbuntuServer:16.04-LTS:16.04.201812070' #Imagen a utilizar en la máquina virtual
 SSH_LLAVE_RUTA='~/.ssh/id_rsa_azure.pub'                  #Ruta del fichero con la llave pública a utilizar
 VM_TAMANO='Standard_B1s'                                  #Tamaño de la máquina virtual
@@ -16,10 +16,10 @@ SSH_PRIORIDAD=100                                         #Tiene que ser diferen
 HTTP_PUERTO=80                                            #NO CAMBIAR, puerto para comunicación vía HTTP
 HTTP_PRIORIDAD=110                                        #Tiene que ser diferente de SSH_PRIORIDAD
 SUBSTRING='PublicIp'                                      #NO CAMBIAR
-VM_INFO_IP_PUBLICO="$VM_NOMBRE_INFO$SUBSTRING"            #NO CAMBIAR, nombre del recurso con la dirección IP pública
-VM_VIAJES_IP_PUBLICO="$VM_NOMBRE_VIAJES$SUBSTRING"        #NO CAMBIAR, nombre del recurso con la dirección IP pública
-DNS_NOMBRE_INFO='ccazureinfo'                             #Nombre DNS a atribuir a la máquina virtual
-DNS_NOMBRE_VIAJES='ccazureviajes'
+VM_INFO_IP_PUBLICO="$VM_NOMBRE_INFO$SUBSTRING"            #NO CAMBIAR, nombre del recurso con la dirección IP pública del microservicio de Información al Cliente
+VM_VIAJES_IP_PUBLICO="$VM_NOMBRE_VIAJES$SUBSTRING"        #NO CAMBIAR, nombre del recurso con la dirección IP pública del microservicio de Gestión de Viajes
+DNS_NOMBRE_INFO='ccazureinfo'                             #Nombre DNS a atribuir a la máquina virtual del microservicio de Información al Cliente
+DNS_NOMBRE_VIAJES='ccazureviajes'                         #Nombre DNS a atribuir a la máquina virtual del microservicio de Gestión de Viajes
 IP_ALOCACION='Dynamic'                                    #Puede ser 'Dynamic' o 'Static'
 PLAYBOOK_RUTA='/etc/ansible/playbook.yml'                 #Ruta del fichero con el playbook Ansible
 
@@ -29,18 +29,18 @@ echo "Creando grupo de recursos $GRUPO_NOMBRE..."
 az group create -l $REGION -n $GRUPO_NOMBRE
 
 echo "Grupo de recursos $GRUPO_NOMBRE creado."
-echo "Creando una máquina virtual Azure..."
+echo "Creando dos máquinas virtuales Azure..."
 
 az vm create -g $GRUPO_NOMBRE -n $VM_NOMBRE_INFO --image $IMAGEN --ssh-key-value $SSH_LLAVE_RUTA --size $VM_TAMANO --data-disk-sizes-gb $DISCO_DATOS_TAMANO
 az vm create -g $GRUPO_NOMBRE -n $VM_NOMBRE_VIAJES --image $IMAGEN --ssh-key-value $SSH_LLAVE_RUTA --size $VM_TAMANO --data-disk-sizes-gb $DISCO_DATOS_TAMANO
 
-echo "Máquina virtual $VM_NOMBRE creada."
-echo "Arrancando máquina virtual $VM_NOMBRE..."
+echo "Máquinas virtuales creadas."
+echo "Arrancando máquinas virtuales..."
 
 az vm start -g $GRUPO_NOMBRE -n $VM_NOMBRE_INFO
 az vm start -g $GRUPO_NOMBRE -n $VM_NOMBRE_VIAJES
 
-echo "Máquina virtual $VM_NOMBRE arrancada."
+echo "Máquinas virtuales arrancadas."
 echo "Abriendo puerto $SSH_PUERTO a la Internet..."
 
 az vm open-port -g $GRUPO_NOMBRE -n $VM_NOMBRE_INFO --port $SSH_PUERTO --priority $SSH_PRIORIDAD
@@ -53,12 +53,12 @@ az vm open-port -g $GRUPO_NOMBRE -n $VM_NOMBRE_INFO --port $HTTP_PUERTO --priori
 az vm open-port -g $GRUPO_NOMBRE -n $VM_NOMBRE_VIAJES --port $HTTP_PUERTO --priority $HTTP_PRIORIDAD
 
 echo "Puerto $HTTP_PUERTO abierto a la Internet."
-echo "Añadiendo nombre DNS $DNS_NOMBRE a la máquina virtual $VM_NOMBRE..."
+echo "Añadiendo nombres DNS a las máquinas virtuales..."
 
 az network public-ip update -g $GRUPO_NOMBRE -n $VM_INFO_IP_PUBLICO --dns-name $DNS_NOMBRE_INFO --allocation-method $IP_ALOCACION
 az network public-ip update -g $GRUPO_NOMBRE -n $VM_VIAJES_IP_PUBLICO --dns-name $DNS_NOMBRE_VIAJES --allocation-method $IP_ALOCACION
 
-echo "Nombre DNS $DNS_NOMBRE añadido a la máquina virtual $VM_NOMBRE"
-echo "Provisionando máquina virtual $VM_NOMBRE..."
+echo "Nombres DNS añadidos a las máquinas virtuales"
+echo "Provisionando máquinas virtuales..."
 
 ansible-playbook $PLAYBOOK_RUTA
